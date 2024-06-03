a27a22802ac08274addc1d2a1974d24d0f61146b
Andros924 <sir_vivi@hotmail.it>
2024-06-03 10:53:11 +0200

    primi esercizi

---
+++ componets2/src/assets/App.jsx
@@ -0,0 +1,11 @@
+import Hello from './Hello'; // Importa il componente Hello
+
+function App() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ componets2/src/assets/Hello.jsx
@@ -0,0 +1,12 @@
+function Hello() {
+    return (
+      <>
+      <h2>Hello, wolrd!</h2>
+      <p>What a beautiful day!</p>
+      
+      </>
+    );
+  }
+  
+  export default Hello;
+  
