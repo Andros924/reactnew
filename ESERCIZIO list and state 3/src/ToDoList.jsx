8bdfb73f495b2fcc7d721e27db7579d84f9d9276
Andros924 <sir_vivi@hotmail.it>
2024-06-15 19:20:12 +0200

    esercizio list

---
+++ ESERCIZIO list and state 3/src/ToDoList.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const addTodo = () => {
+    setTodos([...todos, newTodo]);
+    setNewTodo('');
+  };
+
+  const removeTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <input type="text" value={newTodo} onChange={handleInputChange} />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
